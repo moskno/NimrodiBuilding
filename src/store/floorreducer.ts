f2b1d2a9bea3775f6b7f855aecb09e889ece3dda
Moshe <moskno1@gmail.com>
2024-11-07 11:38:27 +0200

    mission 3.1

--- src/store/floorreducer.ts
+++ src/store/floorreducer.ts
@@ -1,1 +1,35 @@
 //FILL HERE 3.1
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+interface FloorState {
+    floors: {
+        id: number;
+        hasAccess: boolean;
+    }[];
+}
+
+const initialState: FloorState = {
+    floors: [
+        { id: 1, hasAccess: false},
+        { id: 2, hasAccess: false},
+        { id: 3, hasAccess: false},
+        { id: 4, hasAccess: false},
+        { id: 5, hasAccess: false},
+    ]
+}
+
+const floorSlice = createSlice({
+    name: 'floor',
+    initialState,
+    reducers:{
+        changeAccess(state, action: PayloadAction<number>){
+            const floor = state.floors.find((f)=> f.id === action.payload);
+            if(floor){
+                floor.hasAccess = !floor.hasAccess;
+            }
+        }
+    }
+})
+
+export const {changeAccess} = floorSlice.actions;
+export default floorSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,1 +1,14 @@
 // FILL HERE 3.3
+import { configureStore } from "@reduxjs/toolkit";
+import floorReducer from "../store/floorreducer";
+
+const store = configureStore({
+  reducer: {
+    floor: floorReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AddDispatch = typeof store.dispatch;
+
+export default store;
