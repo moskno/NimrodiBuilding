1378968806aeaa495a594c2a345b20bb8d3592c4
Moshe <moskno1@gmail.com>
2024-11-07 11:02:07 +0200

    mission 3.5 + 3.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Reception from "./pages/Reception/Reception";
 import Floor from "./pages/Floor/Floor";
 import PrivateRoute from "./utils/PrivateRoute";
 import Layout from "./components/Layout/Layout";
 import Forbidden from "./pages/Forbidden/Forbidden";
 
-const router = createBrowserRouter([
+const App: React.FC = () => {
  //FILL HERE
-]);
+ return (
+   <Router>
+    <Layout>
+      <Routes>
+        <Route path='/reception' element={<Reception/>}/>
+        <Route path='/floor' element={<PrivateRoute component={<Floor/>}/>}/>
+        <Route path='/forbidden' element={<Forbidden/>}/>
+        <Route path='*' element={<Navigate to='/reception' replace/>}/>
+      </Routes>
+    </Layout>
+   </Router>
+ );
+};
 
-function App() {
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
-}
+
 
 export default App;
--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -1,13 +1,27 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router";
+import { useNavigate } from "react-router-dom";
+import { RootState } from "../store/store";
 
-interface IPrivateRoute{
-    component: ReactNode,
+interface IPrivateRoute {
+  component: ReactNode;
 }
 
-const PrivateRoute = ({ component}:IPrivateRoute) => {
-    //FILL HERE 3.6
+const PrivateRoute: React.FC<IPrivateRoute> = ({ component }) => {
+  //FILL HERE 3.6
+  const navigate = useNavigate();
+
+  const hasAccess = useSelector(
+    (state: RootState) => state.floorReducer.hasAccess
+  );
+
+  useEffect(() => {
+    if (!hasAccess) {
+      navigate("/forbidden");
+    }
+  }, [hasAccess, navigate]);
+
+  return hasAccess ? <>{component}</> : null;
 };
 
-export default PrivateRoute
+export default PrivateRoute;
